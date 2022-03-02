7f116c567a6a841e36aa444b82b0395971ed8cd6
Ronel Peter <ronel.sajan@pm.me>
2022-03-02 07:24:41 -0600

    working on search feature

--- bazzar-world/src/app/app.module.ts
+++ bazzar-world/src/app/app.module.ts
@@ -7,12 +7,24 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routes : Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo:'/products', pathMatch:'full'},
+  {path: '**', redirectTo:'/products', pathMatch:'full'} //! generic wildcard
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
--- bazzar-world/src/app/components/product-list/product-list.component.ts
+++ bazzar-world/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -15,14 +16,28 @@ export class ProductListComponent implements OnInit {
   // productService: any; // ! ?
   
   products!: Product[];
+  currentCategoryId!: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(){
+    this.route.paramMap.subscribe(() => {
     this.listProducts();
+    });
   }
 
   listProducts(){
+
+    // ! check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    // if (hasCategoryId) {
+    //   // ! get the id param string, convert string to number using plus symbol
+    //   this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    // }
+
+
     this.productService.getProductList().subscribe(
         //(    data: Product[]) => { // ! infer?
         data => {
